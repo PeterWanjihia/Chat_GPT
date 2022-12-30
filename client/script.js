856dfe598736b0e55cbc4c10f360f86ffb4041d0
PeterWanjihia <peter.wanjihia@student.moringaschool.com>
2022-12-30 13:36:58 +0300

    defining the submit function

--- client/script.js
+++ client/script.js
@@ -1,46 +1,82 @@
-import bot from './assets/bot.svg';
-import user from './assets/user.svg';
+import bot from "./assets/bot.svg";
+import user from "./assets/user.svg";
 
-const form =document.querySelector('form');
-const chatContainer = document.getElementById('chat_container');
+const form = document.querySelector("form");
+const chatContainer = document.getElementById("chat_container");
 
 let loadInterval;
 
-
 //  This function makes the ... appear in sequence before the texts in the answer appear/ as the answer loads
-function loader(element){
-  element.textContent='';
+function loader(element) {
+  element.textContent = "";
 
   loadInterval = setInterval(() => {
-    element.textContent='.';
+    element.textContent = ".";
 
-    if (element.textContent ==='....'){
-      element.textContent ='';
-    }    
+    if (element.textContent === "....") {
+      element.textContent = "";
+    }
   }, 300);
 }
 
-// This function makes the characters in the answer to appear one by one
-function typeText(element,text){
-  let index =0;
+// Simulating the process of typing
+function typeText(element, text) {
+  let index = 0;
 
-  let interval = setInterval(()=>{
-    if (index<text.length){
+  let interval = setInterval(() => {
+    if (index < text.length) {
       element.innerHTML += text.charAt(index);
-      index++;      
-    }else{
+      index++;
+    } else {
       clearInterval(interval);
     }
-  },20)
-  
+  }, 20);
 }
 
-function generateUniqueID(){
-  const timestamp=Date.now();
+function generateUniqueID() {
+  const timestamp = Date.now();
   const randomNum = Math.random();
   const hexadecimalString = randomNum.toString(16);
 
   return `id-${timestamp}-${hexadecimalString}`;
 }
 
+// Implementing the chat stripe
+function chatStripe(isAI, value, uniqueId) {
+  return `
+      <div class="wrapper ${isAI && "ai"}">
+        <div class="chat">
+          <div class="Profile">
+            <img
+              src="${isAI ? bot : user}"
+              alt="${isAI ? "bot" : "user"}"
+            />
+          </div>
+          <div class="message" id=${uniqueId}>${value}</div>
+        </div>
+      </div>
+    `;
+}
+
+// Implementing the submit button
+const handleSubmit = async (e) => {
+  e.preventDefault();
+
+  const data = new FormData(form);
+  // user's Chatstripe
+  chatContainer.innerHTML += chatStripe(false, data.get("prompt"));
+
+  form.reset();
+
+  // bot's Chatstripe
+  const uniqueId = getUniqueID();
+  chatContainer.innerHTML += chatStripe(true, " ", uniqueId);
+
+  chatContainer.scrollTop = chatContainer.scrollHeight;
+
+  const messageDiv = document.getElementById(uniqueId);
+  loader(messageDiv);
+};
+
+// calling the handleSubmit() and defining the enter event
 
