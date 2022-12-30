fc88052bc3a273531901cd8341cc69d1ea3b85e5
PeterWanjihia <peter.wanjihia@student.moringaschool.com>
2022-12-30 11:58:57 +0300

    simulating typing in answers as well as unique id generation in answers

--- client/script.js
+++ client/script.js
@@ -6,6 +6,8 @@ const chatContainer = document.getElementById('chat_container');
 
 let loadInterval;
 
+
+//  This function makes the ... appear in sequence before the texts in the answer appear/ as the answer loads
 function loader(element){
   element.textContent='';
 
@@ -17,3 +19,28 @@ function loader(element){
     }    
   }, 300);
 }
+
+// This function makes the characters in the answer to appear one by one
+function typeText(element,text){
+  let index =0;
+
+  let interval = setInterval(()=>{
+    if (index<text.length){
+      element.innerHTML += text.charAt(index);
+      index++;      
+    }else{
+      clearInterval(interval);
+    }
+  },20)
+  
+}
+
+function generateUniqueID(){
+  const timestamp=Date.now();
+  const randomNum = Math.random();
+  const hexadecimalString = randomNum.toString(16);
+
+  return `id-${timestamp}-${hexadecimalString}`;
+}
+
+
